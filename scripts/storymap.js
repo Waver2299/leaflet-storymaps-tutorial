93c4e09ad4f4542fd40f6e96822c7b81a2eb066f
Waver2299 <jonas.rieke@gmx.net>
2026-09-25 12:31:03 +0200

    Update storymap.js
    Cleaning/Restoring

--- scripts/storymap.js
+++ scripts/storymap.js
@@ -146,97 +146,7 @@ $(window).on('load', function() {
     var overlay;  // URL of the overlay for in-focus chapter
     var geoJsonOverlay;
 
-    // Swipe function
-var swipeVorher;
-var swipeNachher;
-var swipeSlider;
-var swipeAktiv = false;
-
-var swipeBounds = [
-  [50.5334928, 7.0799612],
-  [50.5764961, 7.1700220]
-];
-
-function swipeStarten() {
-
-  if (swipeAktiv) {
-    return;
-  }
-
-  // Beide Bilder auf die Karte legen
-  swipeVorher = L.imageOverlay(
-    'media/Pre_RGB.png',
-    swipeBounds
-  ).addTo(map);
-
-  swipeNachher = L.imageOverlay(
-    'media/Post_RGB.png',
-    swipeBounds
-  ).addTo(map);
-
-  // Slider erstellen
-  swipeSlider = document.createElement('input');
-  swipeSlider.type = 'range';
-  swipeSlider.min = 0;
-  swipeSlider.max = 100;
-  swipeSlider.value = 50;
-
-  // Slider über der Karte positionieren
-  swipeSlider.style.position = 'absolute';
-  swipeSlider.style.left = '10%';
-  swipeSlider.style.bottom = '30px';
-  swipeSlider.style.width = '80%';
-  swipeSlider.style.zIndex = '1000';
-
-  map.getContainer().appendChild(swipeSlider);
-
-  // Bild entsprechend der Sliderposition abschneiden
-  function swipeAktualisieren() {
-
-    var image = swipeNachher.getElement();
-
-    if (!image) {
-      return;
-    }
-
-    var position = swipeSlider.value;
-
-    image.style.clipPath =
-      'inset(0 ' + (100 - position) + '% 0 0)';
-  }
-
-  swipeSlider.addEventListener('input', swipeAktualisieren);
-
-  // Start bei 50 / 50
-  swipeAktualisieren();
-
-  swipeAktiv = true;
-}
-
-function swipeBeenden() {
-
-  if (!swipeAktiv) {
-    return;
-  }
-
-  if (swipeVorher) {
-    map.removeLayer(swipeVorher);
-  }
-
-  if (swipeNachher) {
-    map.removeLayer(swipeNachher);
-  }
-
-  if (swipeSlider) {
-    swipeSlider.remove();
-  }
-
-  swipeVorher = null;
-  swipeNachher = null;
-  swipeSlider = null;
-  swipeAktiv = false;
-}
-
+    
     for (i in chapters) {
       var c = chapters[i];
 
@@ -413,15 +323,6 @@ function swipeBeenden() {
           }
 
           var c = chapters[i];
-
-          if (i == 9)
-          {
-            swipeStarten();
-          }
-          else
-          {
-            swipeBeenden();
-          }
           
           // Add chapter's overlay tiles if specified in options
           if (c['Overlay']) {
